e35e2ed379f306000b27b7c246039ce8ea4946b8
bailerong <15929276532@163.com>
2019-03-14 21:42:47 +0800

    引用计数的反对

---
+++ Test.java
@@ -0,0 +1,35 @@
+package count;
+/*
+* JVM参数：-xx ：+PrintGC
+*
+*/
+/*public class Test {
+public Object instance=null;
+private static int _1MB =1024*1024;
+private byte[] bigSize=new byte[2*_1MB];
+
+    public static void testGC() {
+        Test test1=new Test();
+        Test test2=new Test();
+        test1.instance=test2;
+        test2.instance=test1;
+        test1=null;
+        test2=null;
+        //强制JVM进行垃圾回收
+        System.gc();
+    }
+    public static void main(String[] args){
+        testGC();
+    }
+}*/
+public class Test{
+    public static void main(String[] args) {
+        Object o=new Object(){
+            public boolean equals(Object obj){
+                return true;
+            }
+        };
+        System.out.println(o.equals("lele"));
+    }
+}# Test
+# Test
