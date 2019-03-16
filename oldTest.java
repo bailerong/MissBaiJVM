1d157129b0de2022d756e4188ce9ddb703e44160
bailerong <15929276532@163.com>
2019-03-16 18:23:40 +0800

    大对象直接进入老年代测试代码

---
+++ oldTest.java
@@ -0,0 +1,22 @@
+package old;
+/*
+* JVM参数如下：
+* -XX:+PrintGCDetails
+* -XX:+UseSerialGC
+* -Xms20M  -Xmx20M -Xmn10M
+* -XX:SurvivorRatio=8
+*-XX:PretenureSizeThreshold=3145728（此时不能写3M）
+* */
+
+public class oldTest {
+    private static final int _1MB=1024*1024;
+    public static void testAlloaction(){
+        byte[] alloaction;
+        alloaction=new byte[4*_1MB];
+    }
+
+    public static void main(String[] args) {
+        testAlloaction();
+    }
+
+}
