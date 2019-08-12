077bdb80d068e597c53e4bce3fb4466312afdc15
bailerong <38148426+bailerong@users.noreply.github.com>
2019-08-12 22:58:16 +0800

    Update oldTest.java

--- oldTest.java
+++ oldTest.java
@@ -6,8 +6,8 @@ package old;
 * -Xms20M  -Xmx20M -Xmn10M
 * -XX:SurvivorRatio=8
 *-XX:PretenureSizeThreshold=3145728（此时不能写3M）
-* */
-
+*/
+//我们的对象的代码
 public class oldTest {
     private static final int _1MB=1024*1024;
     public static void testAlloaction(){
